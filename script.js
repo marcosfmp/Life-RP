cd5fdbfb2588c56f853f37708a8d2adc24b4744f
marcosfmp <marcoscorporativo18@gmail.com>
2025-07-18 20:36:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// Imagens interativas do LIFE RP
-const images = [
-  { src: "images/bandeira.png", caption: "Bandeira LIFE RP" },        // Imagem 1
-  { src: "images/guardiao.png", caption: "Personagem Guardião" },     // Imagem 2
-  { src: "images/acao.png", caption: "Personagem em ação" },          // Imagem 3
-  { src: "images/elegante.png", caption: "Personagem elegante" }      // Imagem 4
-];
+
